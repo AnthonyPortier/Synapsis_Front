7466f1a5c01f388943ec3cd3114ebfc1cc359e77
Anthony Portier <anthonynicolasportier@gmail.com>
2019-12-18 21:25:52 +0100

    navbar sans design ok avec les links

--- front/src/App.js
+++ front/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Router from './components/Router';
+import NavBar from './components/6_navBar/NavBar'
 
 function App() {
   return (
     <div className="App">
+      <NavBar/>
       <Router/>
     </div>
   );
---
+++ front/src/components/6_navBar/NavBar.js
@@ -0,0 +1,54 @@
+import React,{useState} from 'react'
+import { Link } from 'react-router-dom';
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    Input
+} from 'reactstrap';
+
+const NavBar = (props)=>{
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggle = () => setIsOpen(!isOpen);
+
+    return(
+        <div>
+            <Navbar color="light" light expand="md">
+        <NavbarBrand href="/profilePage">Logo</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="mr-auto" navbar>
+            <Input placeholder='rechercher'
+            id="rechercher"
+            name="rechercher"
+            // value={e.target.value}
+            type="text"
+            // onChange={}
+            />
+            <NavItem>
+              <NavLink href="/ProfilePage">Acceuil</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/ProfilePage">Réseau</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/ProfilePage">Messagerie</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/ProfilePage">Notifications</NavLink>
+            </NavItem>
+          </Nav>
+          <NavLink href="/ProfilePage">Ma fiche</NavLink>
+        </Collapse>
+      </Navbar>
+
+        </div>
+    )
+}
+
+export default NavBar;
