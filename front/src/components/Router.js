ef755ae0b3155e81d240b7f87adf4fa8d354c9a7
Anthony Portier <anthonynicolasportier@gmail.com>
2019-12-18 20:05:47 +0100

    e de profileeeeee ok

--- front/src/components/Router.js
+++ front/src/components/Router.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import LandingPage from './1_landingPage/LandingPage';
 import RegisterPage from './2_registerPage/RegisterPage';
-import EditProfil from './3_editProfile/EditProfil'
-import ProfilPage from './4_profilePage/ProfilPage';
+import EditProfile from './3_editProfile/EditProfile'
+import ProfilePage from './4_profilePage/ProfilePage';
 
 
 
@@ -13,8 +13,8 @@ const Router =()=>{
                 <Switch>                    
                     <Route exact path='/' component={RegisterPage}/>
                     <Route exact path='/landingPage' component={LandingPage}/>
-                    <Route exact path='/editProfil' component={EditProfil}/>
-                    <Route exact path='/profilPage' component={ProfilPage}/>
+                    <Route exact path='/editProfile' component={EditProfile}/>
+                    <Route exact path='/profilePage' component={ProfilePage}/>
                 </Switch>
         </div>
     )
