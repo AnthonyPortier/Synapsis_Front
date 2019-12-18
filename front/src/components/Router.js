c0f6ac559d36436392bcd84cefefb8b67e513caf
Anthony Portier <anthonynicolasportier@gmail.com>
2019-12-18 18:13:58 +0100

    browser router ok

--- front/src/components/Router.js
+++ front/src/components/Router.js
@@ -1,25 +1,21 @@
 import React from 'react';
-import { Switch, Route,BrowserRouter } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import LandingPage from './1_landingPage/LandingPage';
 import RegisterPage from './2_registerPage/RegisterPage';
-import EditProfil from './3_editProfil/EditProfil'
-import ProfilPage from './4_profilPage/ProfilPage';
-import SearchBar from './5_searchBar/SearchBar';
+import EditProfil from './3_editProfile/EditProfil'
+import ProfilPage from './4_profilePage/ProfilPage';
 
 
 
 const Router =()=>{
     return(
         <div>
-            <BrowserRouter>
                 <Switch>
                     <Route exact path='/' component={LandingPage}/>
                     <Route exact path='/registerPage' component={RegisterPage}/>
                     <Route exact path='/editProfil' component={EditProfil}/>
                     <Route exact path='/profilPage' component={ProfilPage}/>
-                    <Route exact path='/searchBar' component={SearchBar}/>
                 </Switch>
-            </BrowserRouter>
         </div>
     )
 }
