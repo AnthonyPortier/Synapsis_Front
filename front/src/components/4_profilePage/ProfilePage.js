bb152e48fd7b930972086f65d32fb6e1cd8d8847
kinou-wild <kevin.thomann13@gmail.com>
2019-12-19 03:25:20 +0100

    commencement design profil_joueur (reste gros taf)

--- front/src/components/4_profilePage/ProfilePage.js
+++ front/src/components/4_profilePage/ProfilePage.js
@@ -1,15 +1,36 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 import NavBar from '../6_navBar/NavBar'
+import './ProfilePage.css'
 
 
-const ProfilePage = ()=>{
+const ProfilePage = () => {
 
 
-    return(
-        <div>
-            <NavBar/>
+    return (
+
+        <div className="profile-page">
+
+            <NavBar />
+
+            <div className="profile-div-80">
+
+                <div className="profile-first-container">
+                    <div className="baneer-profile">
+                        <img className="baneer-img" src="https://previews.123rf.com/images/stockdeca/stockdeca1708/stockdeca170800021/85308521-ballon-de-football-sur-l-herbe-verte-banni%C3%A8re-illustration-de-rendu-3d.jpg"/>
+                    </div>
+                    <div className="div-info-head-profile">
+                        <p>Rida LAMRINI</p>
+                        <p>Joueur</p>
+                        <p>Montpellier Herault Sport Club</p>
+                    </div>
+                    <div className="div-numberRelation-profile">
+                        <p>500 Relations</p>
+                    </div>
 
 
+                </div>
+
+            </div>
         </div>
     )
 }
--- front/src/components/Router.js
+++ front/src/components/Router.js
@@ -3,18 +3,18 @@ import { Switch, Route } from 'react-router-dom';
 import LandingPage from './1_landingPage/LandingPage';
 import RegisterPage from './2_registerPage/RegisterPage';
 import ProfilePage from './4_profilePage/ProfilePage';
-{/*import EditProfile from './3_editProfile/EditProfile'*/}
+{/*import EditProfile from './3_editProfile/EditProfile'*/ }
 
 
-const Router = () =>{
-    return(
+const Router = () => {
+    return (
         <div>
-                <Switch>                    
-                    <Route exact path='/' component={RegisterPage}/>
-                    <Route exact path='/login' component={LandingPage}/>
-                    {/*<Route exact path='/' component={EditProfile}/>*/}
-                    <Route exact path='/profile' component={ProfilePage}/>
-                </Switch>
+            <Switch>
+                <Route exact path='/' component={RegisterPage} />
+                <Route exact path='/login' component={LandingPage} />
+                {/*<Route exact path='/' component={EditProfile}/>*/}
+                <Route exact path='/profile' component={ProfilePage} />
+            </Switch>
         </div>
     )
 }
