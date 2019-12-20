dc75adf9582722c7f0e4e2ce8ec1d8cb6da5e199
adamissam <issamadam15@gmail.com>
2019-12-20 12:02:02 +0100

    create serach bar

--- front/src/components/5_searchBar/SearchBar.js
+++ front/src/components/5_searchBar/SearchBar.js
@@ -6,6 +6,9 @@ import { Link } from 'react-router-dom';
 const SearchBar = ()=>{
     const [idUser, setIdUser] = useState([])
     const [search,setSearch] = useState('')
+    
+    let [ catchResult, setCatchResult]=useState([])
+
     const [catchId,setCatchId] = useState()
 
 
@@ -14,23 +17,43 @@ const SearchBar = ()=>{
     }, [])
       //fetch de la data du user 
     const fetchDataUser = () => {
-    axios.get(`http://localhost:5000/users`)
+    axios.get(`https://synaps3.herokuapp.com/users`)
     .then(res => setIdUser(res.data))
     .catch((err) => console.log(err))
     
     }
 
+    const filtreApi=()=>{
+        idUser.map(x =>{
+            if( search.toLocaleLowerCase() === x.firstname ||  search.toLocaleLowerCase() === x.lastname){
+            setCatchResult([...catchResult, x])
+            }
+            return catchResult
+        } )
+    }
+    useEffect( () => {
+    filtreApi()        
+
+    },[search])
+    const changeHelper = (e) => {
+        setSearch(e.target.value)
+        
+    }
 
     return(
         <div>
                 <form > 
                     <input id="lastname" name="lastname" value={search} type="text"
-                    onChange={(e) => { setSearch(e.target.value) }} />
+                    onChange={(e) =>  changeHelper(e)} />
                 </form>
 
                 {idUser.filter(x => x.lastname.toUpperCase() === search.toUpperCase()).map(x =>
                 <div>
-                    <p>{x.lastname}</p>
+                {
+                catchResult.map(function(item) {
+                return <div key={item.id}> {item.firstname} {item.lastname}</div>
+                })
+                }
                 <button onClick={()=>setCatchId(x.id)}><Link to={`/routes/${catchId}`}>search</Link></button>
                 </div>
             )}                
