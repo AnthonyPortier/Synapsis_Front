f3977b95733495c3cfd07e2eb75773778fa96d67
chojiu <yacine@wildcodeschool.fr>
2019-12-20 05:51:15 +0100

    settings routes

--- src/components/Router.js
+++ src/components/Router.js
@@ -3,7 +3,7 @@ import {Switch, Route } from 'react-router-dom';
 import LandingPage from './1_landingPage/LandingPage';
 import RegisterPage from './2_registerPage/RegisterPage';
 import ProfilePage from './4_profilePage/ProfilePage';
-import Routes from './test/Routes'
+// import Routes from './test/Routes'
 
 
 const Router = () => {
@@ -13,8 +13,7 @@ const Router = () => {
                 <Route exact path='/' component={RegisterPage} />
                 <Route exact path='/login' component={LandingPage} />
                 <Route exact path='/profilepage' component={ProfilePage} />
-                <Route exact path='/routes/:id' component={Routes} />
-                
+                {/* <Route exact path='/routes/:id' component={Routes} /> */}
             </Switch>
         </div>
     )
